f2e1728b4d7ef9592d1bf249498df23403f97201
maximejimenez <jimenezmaxime51@gmail.com>
2014-05-19 23:43:47 +0200

    Project init Visual Studio

---
+++ viamarket/viamarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(viamarket.Startup))]
+namespace viamarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
