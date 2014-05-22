a67b614f6fb9c49253cdae14206742a905938966
maximejimenez <jimenezmaxime51@gmail.com>
2014-05-22 17:13:50 +0200

    Identity model modified

--- viamarket/viamarket/Models/IdentityModels.cs
+++ viamarket/viamarket/Models/IdentityModels.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace viamarket.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
