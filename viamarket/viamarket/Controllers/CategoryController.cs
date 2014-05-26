faae420a59de90d29d179db8bdb090392138c4ca
maximejimenez <jimenezmaxime51@gmail.com>
2014-05-26 22:12:52 +0200

    Category controller

---
+++ viamarket/viamarket/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ViaMarket.DataAccess;
+
+namespace ViaMarket.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        MarketEntities data = new MarketEntities();
+        public IEnumerable<Category> Get()
+        {
+            return data.Category;
+        }
+        public IEnumerable<Item> Get(int id)
+        {
+            return data.Item;
+        }
+        public List<string> getListCategory(){
+            List<string> list = new List<string>();
+            foreach(Category c in this.Get()){
+                list.Add(c.name);
+            }
+            return list;
+        }
+    }
+}
