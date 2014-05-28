85a8fb0c56283765a271b1fc18fa8923170039d8
maximejimenez <jimenezmaxime51@gmail.com>
2014-05-28 15:23:18 +0200

    Add a date to Item

---
+++ viamarket/viamarket/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+namespace ProductsApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int IdCategory { get; set; }
+        public int IdCurrency { get; set; }
+        public DateTime date { get; set; }
+    }
+}
