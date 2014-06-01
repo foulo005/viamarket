eb3e68aeb13cc65757a3800a5390d83ff2f17779
maximejimenez <jimenezmaxime51@gmail.com>
2014-06-01 02:13:24 +0200

    Item create,details,delete,edit

--- viamarket/viamarket/Models/ItemViewModel.cs
+++ viamarket/viamarket/Models/ItemViewModel.cs
@@ -9,6 +9,7 @@ namespace ViaMarket.Models
 {
     public class ItemViewModel : ViewModelBase
     {
+        public int Id { get; set; }
         public List<Category> ListCategories;
         public List<Currency> ListCurrencies;
         public string Title { get; set; }
@@ -16,6 +17,8 @@ namespace ViaMarket.Models
         public double Price { get; set; }
         public int IdCategory { get; set; }
         public int IdCurrency { get; set; }
+        public string Category { get; set; }
+        public string Currency { get; set; }
         public IEnumerable<SelectListItem> Categories {
             get { return new SelectList(ListCategories, "Id", "Name"); }
         }
