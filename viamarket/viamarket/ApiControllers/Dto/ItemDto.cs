a02e8af99d3dbac7c74594ec4c49659cfa238e87
svennyffenegger <sven.nyffenegger@gmail.com>
2014-06-03 16:16:58 +0200

    working on webservice

--- viamarket/viamarket/ApiControllers/Dto/ItemDto.cs
+++ viamarket/viamarket/ApiControllers/Dto/ItemDto.cs
@@ -5,7 +5,17 @@ using System.Web;
 
 namespace ViaMarket.ApiControllers.Dto
 {
-    public class ItemDto
+    public class ItemDto : ItemUpdateDto
+    {
+        public UserDto ApplicationUser { get; set; }
+
+        public CategoryDto Category { get; set; }
+
+        public CurrencyDto Currency { get; set; }
+        public ICollection<ImageDto> Image { get; set; }
+    }
+
+    public class ItemUpdateDto
     {
         public int Id { get; set; }
         public string Title { get; set; }
@@ -15,11 +25,8 @@ namespace ViaMarket.ApiControllers.Dto
         public DateTime Created { get; set; }
         public bool Ongoing { get; set; }
 
-        public UserDto ApplicationUser { get; set; }
-
-        public CategoryDto Category { get; set; }
-
-        public CurrencyDto Currency { get; set; }
-        public ICollection<ImageDto> Image { get; set; }
+        public string IdAspNetUsers { get; set; }
+        public int IdCurrency { get; set; }
+        public int IdCategory { get; set; }
     }
 }
