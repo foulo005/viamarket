4c52834bed1ca8b6cc203f44c4d343bf718921e0
HugoMackpaye <hugo.mackp@gmail.com>
2014-06-04 20:10:35 +0200

    update android

--- viamarket/ViaMarketApp/src/com/Via/market/SearchActivity.java
+++ viamarket/ViaMarketApp/src/com/Via/market/SearchActivity.java
@@ -1,7 +1,8 @@
-
 package com.Via.market;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,14 +10,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Handler.Callback;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,11 +28,39 @@ public class SearchActivity extends Fragment {
 	private EditText searchField;
 	private Button searchButton;
 	private Spinner categoriesSpinner;
+	//List of categories + list for the result
 	private List<String> categoriesList = new ArrayList<String>();
-	//yeah its api/item/category/{id:int}/{search}
-	
+	private List<Item> result = new ArrayList<Item>();
 
-	// HttpRequest
+	// TAG FOR SEARCHING IN JSON NODES
+	private static final String TAG_ID = "Id";
+	private static final String TAG_PRICE = "Price";
+	private static final String TAG_CODE = "Code";
+	private static final String TAG_NAME = "Name";
+	private static final String TAG_TITLE = "Title";
+	private static final String TAG_DESCRIPTION = "Description";
+	private static final String TAG_CREATED = "Created";
+	private static final String TAG_PREVIEW = "PathPreview";
+	private static final String TAG_USER = "ApplicationUser";
+	private static final String TAG_USERNAME = "UserName";
+	private static final String TAG_CATEGORY = "Category";
+	private static final String TAG_CURRENCY = "Currency";
+	private static final String TAG_ONGOING = "Ongoing";
+	private static final String TAG_ORIGINAL = "PathOriginal";
+	// ITEM ATTRIBUTES
+	private String itemID;
+	private String curID;
+	private String catID;
+	private String userID;
+	private String itemTitle;
+	private String itemPrice;
+	private String itemDescription;
+	private String curCode;
+	private String catName;
+	private String creationDate;
+	private String[] images;
+	private String userName;
+	private String ongoing;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,26 +69,29 @@ public class SearchActivity extends Fragment {
 				.inflate(R.layout.activity_search, container, false);
 		Intent i = getActivity().getIntent();
 		String str = i.getStringExtra("lol");
-		if (str !=null){
-			
-		} else{
-		searchField = (EditText) view.findViewById(R.id.searchEditText);
-		searchButton = (Button) view.findViewById(R.id.searchButton);
-		categoriesSpinner = (Spinner) view.findViewById(R.id.categoriesSpinner);
-		ArrayAdapter<String> cat = new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_list_item_1, categoriesList);
-		categoriesSpinner.setAdapter(cat);
-		searchButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-	
-				
-			}
-		});}
+		if (str != null) {
+
+		} else {
+			searchField = (EditText) view.findViewById(R.id.searchEditText);
+			searchButton = (Button) view.findViewById(R.id.searchButton);
+			categoriesSpinner = (Spinner) view
+					.findViewById(R.id.categoriesSpinner);
+			ArrayAdapter<String> cat = new ArrayAdapter<String>(getActivity(),
+					android.R.layout.simple_list_item_1, categoriesList);
+			categoriesSpinner.setAdapter(cat);
+			searchButton.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					String search = searchField.getText().toString();
+					String idCat = categoriesSpinner.getSelectedItem().toString();
+					SearchRequest searchTask = new SearchRequest();
+					searchTask.execute(idCat,search);
+				}
+			});
+		}
 		return view;
-		
+
 	}
 
 	@Override
@@ -112,6 +141,115 @@ public class SearchActivity extends Fragment {
 					}
 				}
 			}
+		}
+	}
+
+	public class SearchRequest extends AsyncTask<String, Void, Boolean> {
+
+		private String json;
+		private ProgressDialog dialog;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			dialog = new ProgressDialog(getActivity());
+			dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			dialog = ProgressDialog.show(getActivity(), "Loading...",
+					"Please wait...", true);
+		}
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			JSONParser jsonParser = new JSONParser();
+			String requestURL = "http://viamarket-001-site1.myasp.net/api/item/category/"+ params[0] +"/"
+					+ params[1];
+			try {
+				URLEncoder.encode(requestURL,"UTF-8");
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				json = jsonParser.loadListItem(requestURL);
+
+				if (json != null)
+					// System.out.println(json);
+					return true;
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return false;
+		}
+		@Override
+		protected void onPostExecute(final Boolean success) {
+			if (success) {
+
+				try {
+					JSONArray jArr = new JSONArray(json);
+					for (int n = 0; n < jArr.length(); n++) {
+						JSONObject jObj = jArr.getJSONObject(n);
+						Item it = ItemFromJson(jObj);
+						result.add(it);
+					}
+					System.out.println(result);
+					dialog.dismiss();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+		public Item ItemFromJson(JSONObject json) throws JSONException {
+			if (json != null) {
+				// UserNode
+				JSONObject user = json.getJSONObject(TAG_USER);
+				userID = user.getString(TAG_ID).toString();
+				userName = user.getString(TAG_USERNAME).toString();
+
+				// CategoryNode
+				JSONObject category = json.getJSONObject(TAG_CATEGORY);
+				catName = category.getString(TAG_NAME);
+				catID = category.getString(TAG_ID);
+
+				// CurrencyNode
+				JSONObject currency = json.getJSONObject(TAG_CURRENCY);
+				curID = currency.getString(TAG_ID);
+				curCode = currency.getString(TAG_CODE);
+
+				// Item
+				itemID = json.getString(TAG_ID);
+				itemPrice = json.getString(TAG_PRICE);
+				itemTitle = json.getString(TAG_TITLE);
+				itemDescription = json.getString(TAG_DESCRIPTION);
+				creationDate = json.getString(TAG_CREATED);
+				ongoing = json.getString(TAG_ONGOING);
+
+				// Images array
+				JSONArray jImage = json.getJSONArray("Image");
+				images = new String[jImage.length() * 3];
+				List<String> temp = new ArrayList<String>();
+				for (int i = 0; i < jImage.length(); i++) {
+					JSONObject jTemp = jImage.getJSONObject(i);
+					temp.add(jTemp.getString(TAG_ID));
+					temp.add(jTemp.getString(TAG_ORIGINAL));
+					temp.add(jTemp.getString(TAG_PREVIEW));
+
+				}
+				images = temp.toArray(new String[temp.size()]);
+
+				Item item = new Item(itemID, itemTitle, itemDescription, itemPrice,
+						creationDate, curID, catID, userID, userName, curCode,
+						catName, ongoing, images);
+
+				return item;
+			}
+			return null;
 		}
 	}
 }
